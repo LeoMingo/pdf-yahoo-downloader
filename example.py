a6b708bff9a2760fb846070198efa5e86ab49e36
Leo Mingo <leomingo@outlook.com>
2017-06-25 18:53:18 +0000

    Add files via upload

--- example.py
+++ example.py
@@ -1,14 +1,23 @@
+#import socket
+#socket.setdefaulttimeout(15)
 import pdf_web_downloader as pwd
+#from urllib import urlretrieve
 
 def main():
     pagehtml = ""
-    for i in xrange(3):
+    for i in xrange(5):
         pagehtml += pwd.get_pagehtml_str(pwd.parse_key_to_search_string("machine learning pdf download", i))
         print "page {0} html download complete".format(str(i))
 
 
     llm = pwd.LinkListManip(pagehtml)
-    llm.download()
+    pl = llm.download_pdf(dest_dir='./dl-pdf/')
+    """
+    for i in pl:
+        urlretrieve(i, i.split('/')[-1])
+        print i.split('/')[-1]
+    """
+
 
 if __name__ == "__main__":
     main()
--- pdf_web_downloader.py
+++ pdf_web_downloader.py
@@ -3,7 +3,7 @@
 #then determine the links of pdfs and download them
 
 from urllib2 import urlopen
-import sys
+import concurrent.futures
 import os
 
 def check_idx_str(str0, str1, idx):
@@ -39,19 +39,29 @@ def get_pagehtml_str(url):
     phtm = urlopen(url)
     return phtm.read()
 
-def download_file(url, filename, dest='./'):
+uncompeleted_pdfs = []
+completed_pdfs = []
+def download_file(url, filename, dest):
     if dest[-1] != '/':
         dest += '/'
     if os.path.isdir(dest):
+        uncompeleted_pdfs.append(dest+filename)
+        print "downloading file \"{0}\"".format(filename)
         f = open(dest+filename, 'wb')
         conts = urlopen(url)
         f.write(conts.read())
         f.close()
-        return
+        print "downloading file \"{0}\" finished".format(filename)
+        completed_pdfs.append(dest+filename)
     else:
         print "Failed to download {0}, \"{1}\"  \
                 does not exist".format(filename, dest)
-        return
+"""
+    print "downloading file \"{0}\"".format(filename)
+    urlretrieve(url, filename)
+    print "downloading file \"{0}\" finished".format(filename)
+"""
+
 
 class LinkListManip(object):
     def __init__(self, phtm):
@@ -92,16 +102,20 @@ class LinkListManip(object):
         tok = ".pdf"
         pdf_links = [pdf_link for pdf_link in self.link_list
                      if pdf_link.split('.')[-1]=='pdf']
+        dests = [dest_dir for i in pdf_links]
         if inc_name==True:
-            cnt=0
-            for pl in pdf_links:
-                download_file(pl, str[cnt]+'.pdf', dest=dest_dir)
-                print "{0}.pdf download complete".format(cnt)
-                cnt += 1
+            pdf_names = [str(i)+'.pdf' for i in xrange(len(pdf_links))]
         else:
-            for pl in pdf_links:
-                download_file(pl, pl.split('/')[-1], dest=dest_dir)
-                print "{0} download complete".format(pl.split('/')[-1])
+            pdf_names = [p.split('/')[-1] for p in pdf_links]
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as TPE:
+            TPE.map(download_file, pdf_links, pdf_names, dests, timeout=15)
+
+        print "PDFs download complete, deleting failed downloads"
+        #Remove downloading failures
+        for p in uncompeleted_pdfs:
+            if p not in completed_pdfs:
+                os.remove(p)
 
 
 
